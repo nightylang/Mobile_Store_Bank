afcc0b0da20a20c39bf44e3cbb04daebcd18ff9c
Nighty Lang <kojmko6@gmail.com>
2026-09-01 22:26:41 +0700

    Update SwaggerPosHeaderFilter.cs

--- SampleCode/Data/SwaggerPosHeaderFilter.cs
+++ SampleCode/Data/SwaggerPosHeaderFilter.cs
@@ -1,7 +1,7 @@
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace MobileStoreBank.Data
+namespace StoreMobile.Data
 {
     public class SwaggerPosHeaderFilter : IOperationFilter
     {
